f9c187728de988b44f122899cae0fb3fbdd29bb5
Miguel Portillo <mipor.278@gmail.com>
2022-08-01 12:08:56 -0500

    first commit

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const initialState = {
+    cart: [],
+    toggleOrders: false
+}
+
+const useInitialState = () => {
+    const [state, setState] = useState(initialState);
+    // const [toggleOrders, setToggleOrders] = useState(false);
+
+    const addToCart = (payload) => {
+        setState({
+            ...state,
+            cart: [...state.cart, payload]
+        })
+    };
+
+    const removeFromCart = (payload) => {
+        setState({
+            ...state,
+            cart: state.cart.filter((product, index) => index !== payload)
+        })
+    }
+
+    const setToggleOrders = (payload) => {
+        setState({
+            ...state,
+            toggleOrders: payload
+        })
+    }
+
+    const getToggleOrders = () => {
+        return state.toggleOrders;
+    }
+
+    return {
+        state,
+        getToggleOrders,
+        setToggleOrders,
+        addToCart,
+        removeFromCart
+    }
+}
+
+export default useInitialState;
